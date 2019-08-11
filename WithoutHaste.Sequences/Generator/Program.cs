c2c2881f3ab5c3def75ed0326bf22e26f54f4a69
J M <wohaste@gmail.com>
2019-08-11 08:17:13 -0600

    additive primes

--- WithoutHaste.Sequences/Generator/Program.cs
+++ WithoutHaste.Sequences/Generator/Program.cs
@@ -9,8 +9,9 @@ namespace Generator
 		{
 			//Sequence sequence = new SieveOfEratosthenes(Int32.MaxValue); //out of memory exception adding 15th sieve
 			//Sequence sequence = new SieveOfEratosthenes(Int32.MaxValue / 2); //ran out of memory getting approx. 33 millionth prime from the sieve
-			Sequence sequence = new SieveOfEratosthenes(200000);
+			//Sequence sequence = new SieveOfEratosthenes(200000);
 			//Sequence sequence = new Happy(200000);
+			Sequence sequence = new AdditivePrime(200000);
 			var n1 = sequence.Numbers;
 			sequence.Save();
 
--- WithoutHaste.Sequences/WithoutHaste.Sequences/AdditivePrime.cs
+++ WithoutHaste.Sequences/WithoutHaste.Sequences/AdditivePrime.cs
@@ -1,40 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using WithoutHaste.Sequences.Tools;
 
 namespace WithoutHaste.Sequences
 {
-	/*
 	/// <summary>
 	/// Prime number where sum of digits is also a prime number.
 	/// </summary>
 	public class AdditivePrime : Sequence
 	{
 		/// <inheritdoc/>
-		public override BigInteger[] Numbers { get { return (BigInteger[])numbers.Clone(); } }
-		private BigInteger[] numbers;
+		internal static new int[] TestNumbers = new int[] { 2, 3, 5, 7, 11, 23, 29, 41, 43, 47, 61, 67, 83, 89, 101, 113, 131, 137, 139, 151, 157, 173, 179, 191, 193, 197, 199, 223, 227, 229, 241, 263, 269, 281, 283, 311, 313, 317, 331, 337, 353, 359, 373, 379, 397, 401, 409, 421, 443, 449 };
 
-		/// <inheritdoc/>
-		internal static new BigInteger[] TestNumbers = new BigInteger[] { 2, 3, 5, 7, 11, 23, 29, 41, 43, 47, 61, 67, 83, 89, 101, 113, 131, 137, 139, 151, 157, 173, 179, 191, 193, 197, 199, 223, 227, 229, 241, 263, 269, 281, 283, 311, 313, 317, 331, 337, 353, 359, 373, 379, 397, 401, 409, 421, 443, 449 };
-
-		public AdditivePrime(BigInteger max) : base(max)
+		public AdditivePrime(int max) : base(max)
 		{
 		}
 
 		protected override void Generate()
 		{
-			BigInteger[] primes = new Prime(Max).Numbers;
-			List<BigInteger> additivePrimes = new List<BigInteger>();
-			foreach(BigInteger p in primes)
+			List<int> primes = (new SieveOfEratosthenes(Max)).Numbers;
+			int maxPreloaded = (Numbers.Count > 0) ? Numbers.Last() : 0;
+			for(int i = 0; i < primes.Count; i++)
 			{
-				BigInteger sumOfDigits = p.SumOfDigits();
-				if(primes.Contains(sumOfDigits))
+				int prime = primes[i];
+				if(prime <= maxPreloaded)
+					continue;
+				int sumOfDigits = prime.SumOfDigits();
+				if(primes.Contains(sumOfDigits)) //sum of digits is guaranteed to be equal or lesser number, so its in the prime collection
 				{
-					additivePrimes.Add(p);
+					Numbers.Add(prime);
 				}
 			}
-			numbers = additivePrimes.ToArray();
+		}
+
+		protected override string GetSaveToFolder()
+		{
+			return "Prime_Additive";
 		}
 	}
-	*/
 }
