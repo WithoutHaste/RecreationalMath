3fcc88d24fffb6196e40646070a9af70a03051cd
J M <wohaste@gmail.com>
2019-08-11 09:22:41 -0600

    higgs primes

--- WithoutHaste.Sequences/Generator/Program.cs
+++ WithoutHaste.Sequences/Generator/Program.cs
@@ -9,16 +9,16 @@ namespace Generator
 		{
 			//Sequence sequence = new SieveOfEratosthenes(Int32.MaxValue); //out of memory exception adding 15th sieve
 			//Sequence sequence = new SieveOfEratosthenes(Int32.MaxValue / 2); //ran out of memory getting approx. 33 millionth prime from the sieve
-			
+
 			//Sequence sequence2 = new SieveOfEratosthenes(2000000);
 			//sequence2.Save();
-			
+
 			//Sequence sequence = new Happy(200000);
 			//Sequence sequence = new AdditivePrime(200000);
 			//Sequence sequence = new CircularPrime(200000);
 			//Sequence sequence = new CousinPrime(200000);
-			Sequence sequence = new EmirpPrime(200000);
-			var n1 = sequence.Numbers;
+			//Sequence sequence = new EmirpPrime(200000);
+			Sequence sequence = new HiggsPrime(200000);
 			sequence.Save();
 
 			Console.WriteLine("Done");
--- WithoutHaste.Sequences/WithoutHaste.Sequences/HiggsPrime.cs
+++ WithoutHaste.Sequences/WithoutHaste.Sequences/HiggsPrime.cs
@@ -1,11 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
+using System.Numerics;
 
 namespace WithoutHaste.Sequences
 {
-	/*
 	/// <summary>
 	/// A Higgs prime, named after Denis Higgs, is a prime number with a totient (one less than the prime) that evenly divides the square of the product of the smaller Higgs primes.
 	/// 
@@ -18,33 +17,45 @@ namespace WithoutHaste.Sequences
 	public class HiggsPrime : Sequence
 	{
 		/// <inheritdoc/>
-		public override BigInteger[] Numbers { get { return (BigInteger[])numbers.Clone(); } }
-		private BigInteger[] numbers;
+		internal static new int[] TestNumbers = new int[] { 2, 3, 5, 7, 11, 13, 19, 23, 29, 31, 37, 43, 47, 53, 59, 61, 67, 71, 79, 101, 107, 127, 131, 139, 149, 151, 157, 173, 181, 191, 197, 199, 211, 223, 229, 263, 269, 277, 283, 311, 317, 331, 347, 349 };
 
-		/// <inheritdoc/>
-		internal static new BigInteger[] TestNumbers = new BigInteger[] { 2, 3, 5, 7, 11, 13, 19, 23, 29, 31, 37, 43, 47, 53, 59, 61, 67, 71, 79, 101, 107, 127, 131, 139, 149, 151, 157, 173, 181, 191, 197, 199, 211, 223, 229, 263, 269, 277, 283, 311, 317, 331, 347, 349 };
-
-		public HiggsPrime(BigInteger max) : base(max)
+		public HiggsPrime(int max) : base(max)
 		{
 		}
 
 		protected override void Generate()
 		{
-			BigInteger[] primes = new Prime(Max).Numbers;
-			List<BigInteger> higgsPrimes = new List<BigInteger>();
-			BigInteger productOfHiggsPrimes = 1;
-			foreach(BigInteger p in primes)
+			Sequence primes = new SieveOfEratosthenes(Max);
+			BigInteger productOfHiggsPrimes = ProductOfNumbers();
+			foreach(int prime in primes.InRange(LastNumber, Max))
 			{
-				BigInteger totient = p - 1;
+				int totient = prime - 1;
 				BigInteger squaredProduct = productOfHiggsPrimes * productOfHiggsPrimes;
 				if(squaredProduct % totient == 0)
 				{
-					higgsPrimes.Add(p);
-					productOfHiggsPrimes *= p;
+					Numbers.Add(prime);
+					productOfHiggsPrimes *= prime;
 				}
 			}
-			numbers = higgsPrimes.ToArray();
+		}
+
+		/// <summary>
+		/// Returns the product of all <see cref='Numbers'/> so far.
+		/// </summary>
+		/// <returns></returns>
+		private BigInteger ProductOfNumbers()
+		{
+			BigInteger product = new BigInteger(1);
+			foreach(int number in Numbers)
+			{
+				product = product * number;
+			}
+			return product;
+		}
+
+		protected override string GetSaveToFolder()
+		{
+			return "Prime_Higgs";
 		}
 	}
-	*/
 }
