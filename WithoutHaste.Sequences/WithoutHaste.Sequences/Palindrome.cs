bd21000b7f4607182bb32cdf152c1690f983722f
J M <wohaste@gmail.com>
2019-08-13 11:10:49 -0600

    organization

---
+++ WithoutHaste.Sequences/WithoutHaste.Sequences/Palindrome.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WithoutHaste.Sequences
+{
+	/// <summary>
+	/// Numbers that remain the same when their decimal digits are read backwards. 
+	/// 
+	/// </summary>
+	class Palindrome
+	{
+	}
+}
